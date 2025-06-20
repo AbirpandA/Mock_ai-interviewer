933621b0199f9c356d5048d20807dc95918014b2
Abir panda <abirpanda335@gmail.com>
2025-06-20 15:26:55 +0530

    created Authentication and connected to the storage

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -9,6 +9,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import Formfield from "./Formfield";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/Client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -31,12 +34,39 @@ const Authform = ({ type }: { type: FormType }) => {
   });
   
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-in") {
+        const {email,password}=values;
+
+        const userCredentials = await signInWithEmailAndPassword(auth,email,password)
+
+        const idToken=await userCredentials.user.getIdToken()
+
+        if(!idToken){
+            toast.error('Sign in failed')
+            return
+        }
+
+        await signIn({email,idToken})
+
         toast.success('Sign In sucessfully');
         router.push('/')
       } else {
+
+        
+        const {name,email,password}=values
+
+        const userCredentials=await createUserWithEmailAndPassword(auth,email,password);
+
+        const result=await signUp({uid: userCredentials.user.uid,name:name!,email,password})
+
+        if(!result?.success){
+            toast.error(result?.message)
+            return
+        }
+
+
         toast.success('Account created  sucessfully');
         router.push('/sign-in')
       }
